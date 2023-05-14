07c43a575bc9a0f4691f39347dc3097053be4f75
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-05-14 03:37:27 -0300

    Aula variaveis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // Variaveis em Rust são imutaveis por padrão
+    // Declaração de variavel padrão
+    let variable: u32 = 34;
+    let variable = 34_u64;
+
+    // Inferencia de tipo
+    let variable = 3.4;
+
+    // Variavel não utilizada, compilador não reclama
+    let _variable_nao_ultilizada = 70;
+
 }

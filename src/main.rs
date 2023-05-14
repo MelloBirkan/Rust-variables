03643d5d848fe5786a803e941354f178c8226b93
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-05-14 03:51:42 -0300

    Aula variaveis

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused_variables)]
+
 fn main() {
     // Variaveis em Rust são imutaveis por padrão
     // Declaração de variavel padrão
@@ -11,7 +12,15 @@ fn main() {
     // Variavel não utilizada, compilador não reclama
     let _variable_nao_ultilizada = 70;
 
-    // Casting
+    // Casting, Rust não faz casting automatico
     let float_thirty_two = 10.2_f32;
     let unsigned_eight: u8 = 12;
+    let cast_unsigned_eight = unsigned_eight as f32;
+    let sum = float_thirty_two + cast_unsigned_eight;
+    println!("{}", sum);
+
+    // cast int(não pode ser float) to char
+    let int_char = 65_u8;
+    let cast_int_char = int_char as char;
+    println!("{}", cast_int_char);
 }
